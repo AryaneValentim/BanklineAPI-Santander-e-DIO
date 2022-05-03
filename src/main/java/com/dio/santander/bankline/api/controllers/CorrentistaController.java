3a9831fe3915192295b73a0c2f749691c979af4a
Aryane Valentim <aryanevalentim@gmail.com>
2022-05-03 18:52:06 -0300

    update camada controller correntista

--- src/main/java/com/dio/santander/bankline/api/controllers/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controllers/CorrentistaController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dio.santander.bankline.api.dto.NovoCorrentista;
 import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.repositories.CorrentistaRepository;
+import com.dio.santander.bankline.api.service.CorrentistaService;
 
 
 
@@ -23,15 +26,17 @@ public class CorrentistaController {
 	@Autowired
 	private CorrentistaRepository correntistaRepository;
 	
+	@Autowired
+	private CorrentistaService correntistaService;
+	
 	@GetMapping
 	public List<Correntista> getAll(){ 
 		return correntistaRepository.findAll();
 	}
 	
 	@PostMapping
-	public void save(@RequestBody Correntista correntista) {
-		
-		return;
+	public void save(@RequestBody NovoCorrentista correntista) {
+		correntistaService.save(correntista);
 	}
 	
 }
