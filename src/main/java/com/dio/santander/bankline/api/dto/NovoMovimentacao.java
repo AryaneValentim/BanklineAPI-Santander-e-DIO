53697f99d8dda6e11bc8891faf5429437355c808
Aryane Valentim <aryanevalentim@gmail.com>
2022-05-04 10:45:45 -0300

    camada dto movimentacao

---
+++ src/main/java/com/dio/santander/bankline/api/dto/NovoMovimentacao.java
@@ -0,0 +1,22 @@
+package com.dio.santander.bankline.api.dto;
+
+import com.dio.santander.bankline.api.model.MovimentacaoTipo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NovoMovimentacao {
+	
+	private String descricao;
+		
+	private Double valor;
+	
+	private MovimentacaoTipo tipo;
+	
+	private Integer idConta;
+	
+}
